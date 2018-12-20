a32f4350de4cf14ed201e81f5635101dc3eb57d9
Jozsef Geng <gengjozsef86@gmail.com>
2018-12-20 17:19:06 +0100

    override Set viewAction

---
+++ src/FondOfSpryker/Zed/Tax/Communication/Controller/SetController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FondOfSpryker\Zed\Tax\Communication\Controller;
+
+use Spryker\Service\UtilText\Model\Url\Url;
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Spryker\Zed\Tax\Communication\Controller\SetController as SprykerSetController;
+
+/**
+ * @method \Spryker\Zed\Tax\Communication\TaxCommunicationFactory getFactory()
+ * @method \Spryker\Zed\Tax\Business\TaxFacadeInterface getFacade()
+ * @method \Spryker\Zed\Tax\Persistence\TaxQueryContainerInterface getQueryContainer()
+ */
+class SetController extends SprykerSetController
+{
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|array
+     */
+    public function viewAction(Request $request)
+    {
+        $idTaxSet = $this->castId($request->query->getInt(static::PARAM_URL_ID_TAX_SET));
+
+        $taxSetTransfer = $this->getFacade()->findTaxSet($idTaxSet);
+
+        if ($taxSetTransfer === null) {
+            $this->addErrorMessage(sprintf('Tax set with id %s doesn\'t exist', $idTaxSet));
+
+            return $this->redirectResponse(static::REDIRECT_URL_DEFAULT);
+        }
+
+        return [
+            'taxSet' => $taxSetTransfer,
+        ];
+    }
+}
